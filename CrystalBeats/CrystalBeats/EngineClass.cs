4a897c092f46c9f74250854f26117f1e124a4005
beatmastersx <beatmastersgit@web.de>
2018-08-29 14:21:25 +0200

    abflug

--- CrystalBeats/CrystalBeats/EngineClass.cs
+++ CrystalBeats/CrystalBeats/EngineClass.cs
@@ -8,25 +8,15 @@ namespace CrystalBeats
 {
     public class Engine
     {
-<<<<<<< HEAD
+
         ProfileClass pProfile;
 
         public Controller cController;
         public Sequencer sSequencer;
-<<<<<<< HEAD
-=======
 
-        ProfileClass pProfile;
-=======
->>>>>>> a8291945fe3b33f0e27d3d74fe1df834c34ab6fc
 
-        public Controller cController;
-        public Sequencer sSequencer;
 
-<<<<<<< HEAD
->>>>>>> Frontend
-=======
->>>>>>> a8291945fe3b33f0e27d3d74fe1df834c34ab6fc
+
         public Engine()
         {
             cController = new Controller();
@@ -40,7 +30,7 @@ namespace CrystalBeats
                 sSequencer.sqBar6,
                 sSequencer.sqBar7,
                 sSequencer.sqBar8};
-            pProfile = new ProfileClass(sequences);
+
         }
     }
 }
